7b29a5688f3bf3ec47c3d61ed2e4962785dac46e
Anegatrac15 <68756609+Anegatrac15@users.noreply.github.com>
2020-09-25 20:03:11 -0500

    Add files via upload

---
+++ 25-SEPT/script.js
@@ -0,0 +1,62 @@
+function hazza() {
+    let name= prompt("¿quién es este artista?")
+    let nombre = document.getElementById("nombre")
+    nombre.textContent = name 
+    
+    let gender= prompt("¿Cual es su genero musical:?")
+    let genero = document.getElementById("genero")
+    genero.textContent = gender
+
+    let country= prompt("¿De que pais es:?")
+    let pais = document.getElementById("pais")
+    pais.textContent = country 
+
+    let song= prompt("¿Que cancion conoces:?")
+    let cancion= document.getElementById("cancion")
+    cancion.textContent = song 
+
+
+
+}
+
+function Mitch() {
+    let name= prompt("¿quién es este artista?")
+    let nombre1 = document.getElementById("nombre1")
+    nombre1.textContent = name 
+    
+    let gender= prompt("¿Cual es su genero musical:?")
+    let genero1 = document.getElementById("genero1")
+    genero1.textContent = gender
+
+    let country= prompt("¿De que pais es:?")
+    let pais1 = document.getElementById("pais1")
+    pais1.textContent = country 
+
+    let song= prompt("¿Que cancion conoces:?")
+    let cancion1= document.getElementById("cancion1")
+    cancion1.textContent = song 
+
+
+
+}
+
+function Cepeda() {
+    let name= prompt("¿quién es este artista?")
+    let nombre2= document.getElementById("nombre2")
+    nombre2.textContent = name 
+    
+    let gender= prompt("¿Cual es su genero musical:?")
+    let genero2 = document.getElementById("genero2")
+    genero2.textContent = gender
+
+    let country= prompt("¿De que pais es:?")
+    let pais2 = document.getElementById("pais2")
+    pais2.textContent = country 
+
+    let song= prompt("¿Que cancion conoces:?")
+    let cancion2= document.getElementById("cancion2")
+    cancion2.textContent = song 
+
+
+
+}
